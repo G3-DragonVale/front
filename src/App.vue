10c3a9564e699e117a8edc30f9c93584a471f01b
Célian CHAUSSON <celian.chausson@etu.univ-smb.fr>
2025-06-05 07:00:43 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,13 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <RouterView />
+  <div class="min-h-screen flex flex-col">
+    <!-- <Navigation /> -->
+
+    <div class="flex flex-grow bg-white">
+      <RouterView class="flex-grow" />
+    </div>
+
+    <!-- <Footer /> -->
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const count = ref(0);
 </script>
 
 <template>
   <main>
-    <p class="text-red-500">Coucou</p>
+    <p class="text-red-500" @click="count++">{{ count }}</p>
   </main>
 </template>
