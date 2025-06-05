a0686a51ecb9cc3d1bbb8ac6119f484af4bc488f
Célian CHAUSSON <celian.chausson@etu.univ-smb.fr>
2025-06-05 06:44:17 +0200

    cleanup and add tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p class="text-red-500">Coucou</p>
   </main>
 </template>
